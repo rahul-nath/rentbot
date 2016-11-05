5ebbefbf43a0f2a7c4b9b959b8194ef97a5cc0eb
afumagalli <anthony.fumagalli@gmail.com>
2016-11-05 14:06:08 -0700

    Refactor as a Flask app

---
+++ __init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, make_response
+app = Flask(__name__)
+app.secret_key = 'super_secret_key'
+
+from pb_py import main as pbApi
+host = 'aiaas.pandorabots.com'
+from secret import app_id, user_key, botname
+conversation = []
+
+@app.route('/', methods=['GET', 'POST'])
+def showMain():
+	'''Shows the main page'''
+	if request.method == 'GET':
+		return render_template('main.html', conversation=conversation)
+	if request.method == 'POST':
+		question = request.form.get('question', '')
+		response = pbApi.talk(user_key, app_id, host, botname, question)
+		conversation.append((question, response['response']))
+		return render_template('main.html', conversation=conversation)
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0', port=5000)
